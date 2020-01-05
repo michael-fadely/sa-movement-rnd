321acbbfa7b8ac9f3dee3d2c18ebd2e79ef3a00f
Michael Fadely <michaelwfadely@gmail.com>
2020-01-05 11:45:41 -0700

    cleanup, simplification

--- sadx-movement-rnd/mod.cpp
+++ sadx-movement-rnd/mod.cpp
@@ -1,88 +1,83 @@
+// ReSharper disable CppClangTidyCppcoreguidelinesAvoidGoto
+// ReSharper disable CppClangTidyHicppAvoidGoto
+// 
 #include <SADXModLoader.h>
 #include <cmath>
 
-#pragma pack(push, 8)
-struct __declspec(align(4)) charobj2_arraything
-{
-  float CollisionRadius;
-  NJS_VECTOR PositionOffset;
-  NJS_VECTOR njs_vector10;
-  NJS_VECTOR njs_vector1C;
-  __int16 SomeIndex1;
-  short SomeIndex2;
-  char gap2C[8];
-  _DWORD RotationX;
-  _DWORD RotationZ;
-  NJS_VECTOR njs_vector3C;
-  NJS_VECTOR SurfaceNormal;
-  NJS_VECTOR njs_vector54;
-  NJS_VECTOR njs_vector60;
-  NJS_VECTOR njs_vector6C;
-};
-#pragma pack(pop)
-
-
 #pragma pack(push, 1)
+struct charobj2_arraything
+{
+	float CollisionRadius;
+	NJS_VECTOR PositionOffset;
+	NJS_VECTOR njs_vector10;
+	NJS_VECTOR njs_vector1C;
+	__int16 SomeIndex1;
+	short SomeIndex2;
+	char gap2C[8];
+	_DWORD RotationX;
+	_DWORD RotationZ;
+	NJS_VECTOR njs_vector3C;
+	NJS_VECTOR SurfaceNormal;
+	NJS_VECTOR njs_vector54;
+	NJS_VECTOR njs_vector60;
+	NJS_VECTOR njs_vector6C;
+};
+
 struct CharObj2_
 {
-  float SpindashSpeed;
-  __int16 Upgrades;
-  __int16 Powerups;
-  __int16 JumpTime;
-  __int16 field_A;
-  __int16 UnderwaterTime;
-  __int16 IdleTime;
-  __int16 StatusBackup;
-  int field_12;
-  char gap16[2];
-  float LoopDist;
-  float Up;
-  NJS_VECTOR SomeKindOfSpeedOffset;
-  NJS_VECTOR AddVelocity;
-  NJS_VECTOR Speed;
-  NJS_VECTOR field_44;
-  NJS_VECTOR SurfaceNormal;
-  int SurfaceFlags;
-  int SurfaceFlags_Old;
-  charobj2_arraything *array_1x132;
-  ObjectMaster *ObjectHeld;
-  void *field_6C;
-  void *SomePointer;
-  int field_74;
-  void *array_15x32;
-  __int16 SonicSpinTimeProbably;
-  __int16 SonicSpinTimer;
-  __int16 LightdashTime;
-  __int16 LightdashTimer;
-  int field_84;
-  NJS_VECTOR field_88;
-  float SomeFrameNumberThing;
-  float TailsFlightTime;
-  PhysicsData PhysicsData;
-  AnimThing AnimationThing;
-  NJS_VECTOR SoManyVectors[12];
-  struct_a3 _struct_a3;
+	float SpindashSpeed;
+	__int16 Upgrades;
+	__int16 Powerups;
+	__int16 JumpTime;
+	__int16 field_A;
+	__int16 UnderwaterTime;
+	__int16 IdleTime;
+	__int16 StatusBackup;
+	int field_12;
+	char gap16[2];
+	float LoopDist;
+	float Up;
+	NJS_VECTOR SomeKindOfSpeedOffset;
+	NJS_VECTOR AddVelocity;
+	NJS_VECTOR Speed;
+	NJS_VECTOR field_44;
+	NJS_VECTOR SurfaceNormal;
+	int SurfaceFlags;
+	int SurfaceFlags_Old;
+	charobj2_arraything* array_1x132;
+	ObjectMaster* ObjectHeld;
+	void* field_6C;
+	void* SomePointer;
+	int field_74;
+	void* array_15x32;
+	__int16 SonicSpinTimeProbably;
+	__int16 SonicSpinTimer;
+	__int16 LightdashTime;
+	__int16 LightdashTimer;
+	int field_84;
+	NJS_VECTOR field_88;
+	float SomeFrameNumberThing;
+	float TailsFlightTime;
+	PhysicsData PhysicsData;
+	AnimThing AnimationThing;
+	NJS_VECTOR SoManyVectors[12];
+	struct_a3 _struct_a3;
 };
-#pragma pack(pop)
 
-//static_assert(sizeof(CharObj2_) == 0x200, "YOU FUCK");
-
-#pragma pack(push, 8)
 struct __declspec(align(4)) EntityData2_
 {
-  CharObj2 *CharacterData;
-  NJS_VECTOR VelocityDirection;
-  NJS_VECTOR SomeCollisionVector;
-  Rotation3 Forward;
-  int field_28;
-  int field_2C;
-  int field_30;
-  float field_34;
-  float field_38;
-  float AccelerationMultiplier;
+	CharObj2* CharacterData;
+	NJS_VECTOR VelocityDirection;
+	NJS_VECTOR SomeCollisionVector;
+	Rotation3 Forward;
+	int field_28;
+	int field_2C;
+	int field_30;
+	float field_34;
+	float field_38;
+	float AccelerationMultiplier;
 };
 #pragma pack(pop)
-
 
 FunctionPointer(float, sub_447510, (EntityData1 *arg0, EntityData2_ *arg4, CharObj2_ *a3), 0x00447510);
 FunctionPointer(float, sub_447010, (EntityData1 *data1, EntityData2_ *data2_pp, CharObj2_ *data2), 0x447010);
@@ -90,10 +85,9 @@ FunctionPointer(void, sub_43EC00, (EntityData1 *a1, NJS_VECTOR *vd), 0x43EC00);
 FunctionPointer(void, sub_4491E0, (EntityData1 *a1, EntityData2_ *a2, CharObj2_ *a3), 0x4491E0);
 FastcallFunctionPointer(double, sub_7889F0, (NJS_VECTOR *a1, NJS_VECTOR *a2, NJS_VECTOR *a3), 0x7889F0);
 
-// float __usercall sub_44B0E0@<eax>(EntityData1 *a1@<ecx>, int eax0@<eax>, float arg_0)
-
 void sub_44B0E0(EntityData1* a1, CharObj2_* eax0, EntityData2_* arg_0)
 {
+	// ReSharper disable once CppDeclaratorNeverUsed
 	auto func = reinterpret_cast<void*>(0x44B0E0);
 
 	__asm
@@ -108,9 +102,9 @@ void sub_44B0E0(EntityData1* a1, CharObj2_* eax0, EntityData2_* arg_0)
 	}
 }
 
-// void __usercall sub_449380(EntityData1 *a1@<eax>, EntityData2_ *a2@<esi>, CharObj2 *a3)
 void sub_449380(EntityData1* a1, EntityData2_* a2, CharObj2_* a3)
 {
+	// ReSharper disable once CppDeclaratorNeverUsed
 	auto func = reinterpret_cast<void*>(0x449380);
 
 	__asm
@@ -124,9 +118,9 @@ void sub_449380(EntityData1* a1, EntityData2_* a2, CharObj2_* a3)
 	}
 }
 
-// void __usercall sub_443DF0(CharObj2 *a1@<eax>, EntityData1 *entity@<edi>, EntityData2_ *a3, __int16 bams)
 void sub_443DF0(CharObj2_* a1, EntityData1* entity, EntityData2_* a3, __int16 bams)
 {
+	// ReSharper disable once CppDeclaratorNeverUsed
 	auto func = reinterpret_cast<void*>(0x443DF0);
 
 	__asm
@@ -141,9 +135,9 @@ void sub_443DF0(CharObj2_* a1, EntityData1* entity, EntityData2_* a3, __int16 ba
 	}
 }
 
-// void __usercall sub_443E60(EntityData1 *a1@<ebx>, CharObj2 *a2@<esi>, EntityData2_ *a3, unsigned __int16 a4)
 void sub_443E60(EntityData1 *a1, CharObj2_ *a2, EntityData2_ *a3, unsigned __int16 a4)
 {
+	// ReSharper disable once CppDeclaratorNeverUsed
 	auto func = reinterpret_cast<void*>(0x443E60);
 
 	__asm
@@ -158,9 +152,9 @@ void sub_443E60(EntityData1 *a1, CharObj2_ *a2, EntityData2_ *a3, unsigned __int
 	}
 }
 
-// void __usercall sub_443C50(CharObj2 *charobj2@<edi>, EntityData1 *data1@<esi>, EntityData2_ *data2, int a4)
 void sub_443C50(CharObj2_ *charobj2, EntityData1 *data1, EntityData2_ *data2, int a4)
 {
+	// ReSharper disable once CppDeclaratorNeverUsed
 	auto func = reinterpret_cast<void*>(0x443C50);
 
 	__asm
@@ -178,13 +172,6 @@ void sub_443C50(CharObj2_ *charobj2, EntityData1 *data1, EntityData2_ *data2, in
 
 void __cdecl HandleFloorVelocity(EntityData1* entity, EntityData2_* a2, CharObj2_* charobj)
 {
-	EntityData1* v3; // edi
-	//float v4; // st0
-	//float v5; // st1
-	//float v6; // st2
-	//float v7; // st3
-	//float v8; // st4
-	//float v9; // st5
 	float v13; // st7
 	float v14; // ecx
 	float v15; // edx
@@ -192,31 +179,18 @@ void __cdecl HandleFloorVelocity(EntityData1* entity, EntityData2_* a2, CharObj2
 	float v18; // st7
 	float v19; // st7
 	float speed_x_; // st6
-	//float v21; // et1
 	float speed_x; // st6
-	//float v23; // et1
 	float new_speed_x; // st6
-	float new_speed_z; // st6
 	Angle y_rotation; // esi
-	float v30; // st7
-	//float v31; // et1
-	float v32; // st7
-	//float v33; // et1
 	float v37; // st7
 	float v38; // st7
-	//float v39; // et1
 	float analog_magnitude; // st7
-	//float v41; // et1
 	int v48; // ecx
-	EntityData2_* v49; // eax
-	float v50; // ecx
-	float v51; // st7
 	float v52; // st7
 	float v53; // st7
 	float v54; // st7
 	float v55; // st7
 	float v56; // st7
-	NJS_VECTOR* add_velocity; // ebp
 	Angle angle; // [esp+8h] [ebp-2Ch]
 	NJS_VECTOR new_speed; // [esp+10h] [ebp-24h]
 	NJS_VECTOR v64; // [esp+1Ch] [ebp-18h]
@@ -224,8 +198,6 @@ void __cdecl HandleFloorVelocity(EntityData1* entity, EntityData2_* a2, CharObj2
 	float a1a; // [esp+38h] [ebp+4h]
 	float a1b; // [esp+38h] [ebp+4h]
 	float a1c; // [esp+38h] [ebp+4h]
-
-	v3 = entity;
 
 	if (entity->Status & Status_DisableControl)
 	{
@@ -258,6 +230,7 @@ void __cdecl HandleFloorVelocity(EntityData1* entity, EntityData2_* a2, CharObj2
 	v64 = { 0.0f, 0.0f, 0.0f };
 	new_speed.y = v16 + v14;
 	new_speed.z = Gravity.z * charobj->PhysicsData.Gravity + v15;
+
 	sub_43EC00(entity, &new_speed);
 
 	const bool have_analog = GetAnalog(entity, &angle, &analog_magnitude);
@@ -276,8 +249,10 @@ void __cdecl HandleFloorVelocity(EntityData1* entity, EntityData2_* a2, CharObj2
 	if (charobj->Up < -0.40000001f && charobj->Speed.x > 1.16f)
 	{
 		v19 = charobj->PhysicsData.Gravity * 5.0f;
+
 	what:
 		new_speed.y = new_speed.y - v19;
+
 	LABEL_15:
 		v18 = new_speed.z;
 		goto LABEL_16;
@@ -325,7 +300,7 @@ LABEL_16:
 	speed_x_ = charobj->Speed.x;
 	if (have_analog)
 	{
-		if (speed_x_ > charobj->PhysicsData.MaxAccel&& charobj->Up > 0.95999998f)
+		if (speed_x_ > charobj->PhysicsData.MaxAccel && charobj->Up > 0.95999998f)
 		{
 			new_speed_x = (charobj->Speed.x - charobj->PhysicsData.MaxAccel) * charobj->PhysicsData.RollDecel * 1.7f;
 			goto LABEL_43;
@@ -342,7 +317,7 @@ LABEL_16:
 	//v23 = charobj->PhysicsData.MaxAccel;
 	speed_x = charobj->Speed.x;
 
-	// TODO: if (c0 | c3)
+	// if (c0 | c3)
 	if (charobj->Speed.x <= charobj->PhysicsData.MaxAccel)
 	{
 		if (speed_x >= 0.0f)
@@ -360,8 +335,7 @@ LABEL_43:
 
 LABEL_44:
 	new_speed.y = charobj->PhysicsData.GravityAdd * charobj->Speed.y + new_speed.y;
-	new_speed_z = charobj->PhysicsData.HitSpeed * charobj->Speed.z + v18;
-	new_speed.z = new_speed_z;
+	new_speed.z = charobj->PhysicsData.HitSpeed * charobj->Speed.z + v18;
 
 	if (have_analog)
 	{
@@ -373,26 +347,21 @@ LABEL_44:
 		}
 		else
 		{
-			v30 = charobj->Up;
-
-			if (v30 < 0.94)
+			if (charobj->Up < 0.94f)
 			{
 				if (charobj->SurfaceFlags & 0x800000)
 				{
 					sub_449380(entity, a2, charobj);
-					v3 = entity;
 				}
 			}
 
 			y_rotation = angle;
 		}
 
-		v32 = charobj->Speed.x;
-
-		// TODO: if (c0 | c2)
+		// if (c0 | c2)
 		if (charobj->Speed.x < charobj->PhysicsData.MaxAccel)
 		{
-			if (v32 < charobj->PhysicsData.RollCancel)
+			if (charobj->Speed.x < charobj->PhysicsData.RollCancel)
 			{
 				if (analog_magnitude > 0.5f)
 				{
@@ -407,19 +376,19 @@ LABEL_44:
 				if (v37 <= charobj->Speed.x)
 				{
 					analog_magnitude = 0.0;
+
 				LABEL_78:
-					v48 = BAMS_Subtract(v3->Rotation.y, y_rotation);
+					v48 = BAMS_Subtract(entity->Rotation.y, y_rotation);
 
 					if (charobj->Speed.x == 0.0f && v48 > 4096)
 					{
 						a2->Forward.y = y_rotation;
 						analog_magnitude = 0.0f;
-						sub_443DF0(charobj, v3, a2, y_rotation);
+						sub_443DF0(charobj, entity, a2, y_rotation);
 
 					LABEL_96:
-						v50 = analog_magnitude;
 						a2->field_34 = analog_magnitude;
-						v64.x = v50;
+						v64.x = analog_magnitude;
 						goto LABEL_97;
 					}
 
@@ -445,17 +414,16 @@ LABEL_44:
 						{
 							analog_magnitude = analog_magnitude * 0.80000001f;
 						}
-						v49 = a2;
+
 						a2->Forward.y = y_rotation;
 					}
 					else
 					{
-						v49 = a2;
 						a2->Forward.y = y_rotation;
 						analog_magnitude = charobj->PhysicsData.GroundDecel;
 					}
 
-					sub_443C50(charobj, entity, v49, y_rotation);
+					sub_443C50(charobj, entity, a2, y_rotation);
 					goto LABEL_96;
 				}
 				goto LABEL_67;
@@ -478,14 +446,13 @@ LABEL_44:
 
 			//v39 = charobj->Speed.x;
 			//v41 = charobj->PhysicsData.Run1;
-
-			// TODO: if (c0 | c2)
+			// if (c0 | c2)
 			if (charobj->Speed.x < charobj->PhysicsData.Run1)
 			{
 				v38 = analog_magnitude * charobj->PhysicsData.GroundAccel;
 
-				// TODO: if (c0 | c3)
-				//fcomp 0.89999998
+				// fcomp 0.89999998
+				// if (c0 | c3)
 				if (analog_magnitude <= 0.89999998f)
 				{
 					v38 = v38 * 0.30000001f;
@@ -496,7 +463,7 @@ LABEL_44:
 		}
 		else
 		{
-			if (v32 < charobj->PhysicsData.MaxAccel || charobj->Up >= 0.0f)
+			if (charobj->Speed.x < charobj->PhysicsData.MaxAccel || charobj->Up >= 0.0f)
 			{
 				v38 = analog_magnitude * charobj->PhysicsData.GroundAccel * 0.40000001f;
 				goto LABEL_77;
@@ -515,6 +482,7 @@ LABEL_44:
 			{
 				v64.x = -charobj->PhysicsData.GroundDecel;
 			}
+
 			a2->field_34 = 0.0f;
 		}
 		else
@@ -584,15 +552,16 @@ LABEL_97:
 
 		if (new_speed.y < 0.0f)
 		{
-			v51 = charobj->PhysicsData.MinSpeed * a2->AccelerationMultiplier * new_speed.y;
-			a1b = v51;
+			a1b = charobj->PhysicsData.MinSpeed * a2->AccelerationMultiplier * new_speed.y;
 
-			if (v51 > 0.0f && -v64.x > a1b)
+			if (a1b > 0.0f && -v64.x > a1b)
 			{
 				v52 = new_speed.x + v64.x;
+
 				if (v52 < 0.0f)
 				{
 					*(float*)&angle = -a1b;
+
 					if (v52 < *(float*)&angle)
 					{
 						a2->field_38 = v52 - a1b;
@@ -623,9 +592,10 @@ LABEL_97:
 	}
 
 	a1b = charobj->PhysicsData.field_68 * a2->AccelerationMultiplier * new_speed.y;
+
 	if (charobj->Speed.x >= 0.0f || new_speed.x >= 0.050999999f || new_speed.x <= -0.050999999f)
 	{
-		if (a1b > 0.0f && a1b < (float)v64.x)
+		if (a1b > 0.0f && a1b < v64.x)
 		{
 			v52 = new_speed.x + v64.x;
 			goto LABEL_136;
@@ -633,7 +603,7 @@ LABEL_97:
 
 	LABEL_138:
 		if (!have_analog
-		    && charobj->Speed.x <= (float)charobj->PhysicsData.RollCancel
+		    && charobj->Speed.x <= charobj->PhysicsData.RollCancel
 		    && new_speed.x < 0.050999999f
 		    && new_speed.x > -0.050999999f)
 		{
@@ -731,7 +701,7 @@ LABEL_145:
 LABEL_169:
 	new_speed.y = new_speed.y + v64.y;
 
-	add_velocity = &charobj->AddVelocity;
+	auto add_velocity = &charobj->AddVelocity;
 	add_velocity->x = new_speed.x;
 	add_velocity->y = new_speed.y;
 	add_velocity->z = new_speed.z;
